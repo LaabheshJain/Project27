e0d975245f855616767cf0b792f605feeacf6499
LaabheshJain <68288963+LaabheshJain@users.noreply.github.com>
2020-08-14 21:22:15 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,22 @@
+class Roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.width = width;
+      this.height = height;
+      this.x=x;
+      this.y=y;
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      fill(128,128,128);
+      rect(0,0, this.width, this.height);
+      pop();
+    }
+  };
